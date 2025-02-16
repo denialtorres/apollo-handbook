5bd55d5ea53956f7ddf051bc4deb93758e021067
denialtorres <denial.torres@gmail.com>
2025-02-16 15:42:52 -0600

    generate and update the schema from the server

---
+++ src/gql/graphql.ts
@@ -0,0 +1,80 @@
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  /** The `Upload` scalar type represents a file upload. */
+  Upload: { input: any; output: any; }
+};
+
+export type Book = {
+  __typename?: 'Book';
+  id: Scalars['ID']['output'];
+  title: Scalars['String']['output'];
+};
+
+export enum CacheControlScope {
+  Private = 'PRIVATE',
+  Public = 'PUBLIC'
+}
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createBook: Book;
+};
+
+
+export type MutationCreateBookArgs = {
+  title: Scalars['String']['input'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  books: Array<Book>;
+  booksWithTitle: Array<Book>;
+  user: UserResult;
+};
+
+
+export type QueryBooksWithTitleArgs = {
+  title: Scalars['String']['input'];
+};
+
+
+export type QueryUserArgs = {
+  username: Scalars['String']['input'];
+};
+
+export type SuspendedUser = {
+  __typename?: 'SuspendedUser';
+  id: Scalars['ID']['output'];
+  suspensionReason: Scalars['String']['output'];
+  username: Scalars['String']['output'];
+};
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['ID']['output'];
+  username: Scalars['String']['output'];
+};
+
+export type UserResult = SuspendedUser | User;
+
+export type AllBooksQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type AllBooksQuery = { __typename?: 'Query', books: Array<{ __typename?: 'Book', id: string, title: string }> };
+
+
+export const AllBooksDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"allBooks"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"books"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"title"}}]}}]}}]} as unknown as DocumentNode<AllBooksQuery, AllBooksQueryVariables>;
